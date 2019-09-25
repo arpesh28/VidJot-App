8b0374a84b65b6ee48829fe98909cc51bdaaae53
arpesh28 <arpeshgadekar@gmail.com>
2019-09-25 23:19:30 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV == 'production'){
+    module.exports = {mongoURI: 'mongodb://<Desmond>:<Desmond@28>@ds127362.mlab.com:27362/vidjot-app'}
+} else {
+    module.exports == {mongoURI: 'mongodb://localhost/vidjot-dev'}
+}
